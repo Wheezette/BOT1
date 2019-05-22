8913855cc64d172ad608c94b97dc08740b128696
Wheezette <41962633+Wheezette@users.noreply.github.com>
2019-05-22 13:52:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -643,8 +643,8 @@ bot.on("message", async message => {
         if(!args[0]) return message.channel.send(`**(!)** Musisz podać treść propozycji, inaczej nie przejdzie.`)
         const suggestEmbed = new Discord.RichEmbed()
         .setColor("9f59d9")
-        .setAuthor("[SUGESTIA]")
-        .addField("Treść:", suggestContent)
+        //.setAuthor("[SUGESTIA]")
+        //.addField("Treść:", suggestContent)
         .setFooter(`Sugestia wysłana przez ${message.author.tag}.`);
         //message.guild.channels.find(`name`, `${suggestChannel}`).send(suggestEmbed);
         let propozycja = await message.guild.channels.find(`name`, `${suggestChannel}`).send(suggestEmbed);
